456be63b33bde32c916094142ccea824391a61c9
muazkhaledawaja <mkaawaja5000@gmail.com>
2023-01-31 13:48:21 +0200

    sorry

--- config/config.development.ts
+++ config/config.development.ts
@@ -8,7 +8,7 @@ export default () => ({
     host: process.env.DB_HOST || 'localhost',
     port:  process.env.DB_PORT || 5432,
     username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || '123456789**Mm',
+    password: process.env.DB_PASS || ' ',
     database: process.env.DB_NAME_DEVELOPMENT || 'ToDo',
   },
 });
